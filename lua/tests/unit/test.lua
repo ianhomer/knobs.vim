2914df2419027acf7030f6bc72a61e778800f91e
Ian Homer <ian.homer@gmail.com>
2021-10-17 14:19:26 +0100

    Lint lua

--- lua/tests/unit/test.lua
+++ lua/tests/unit/test.lua
@@ -1,50 +1,75 @@
+describe(
+    "knobs",
+    function()
+        setup(
+            function()
+                _G.vim = {
+                    cmd = function()
+                    end,
+                    g = {
+                        knobs_levels = {}
+                    }
+                }
+                knobs = require("knobs")
+                function mockUse(args)
+                    return args
+                end
+                function setKnob(knob)
+                    vim.g["knob_" .. knob] = 1
+                    vim.g["knobs_levels"][knob] = 1
+                end
+            end
+        )
 
-describe("knobs", function()
+        describe(
+            "from package name",
+            function()
+                it(
+                    "when nvim after",
+                    function()
+                        assert.are.equal(knobs.fromPackage("x/x.nvim"), "x")
+                    end
+                )
+                it(
+                    "when vim after",
+                    function()
+                        assert.are.equal(knobs.fromPackage("x/x.vim"), "x")
+                    end
+                )
+                it(
+                    "when nvim before",
+                    function()
+                        assert.are.equal(knobs.fromPackage("x/nvim-x"), "x")
+                    end
+                )
+                it(
+                    "when vim before",
+                    function()
+                        assert.are.equal(knobs.fromPackage("x/vim-x"), "x")
+                    end
+                )
+            end
+        )
 
-  setup(function()
-    _G.vim = {
-      cmd = function() end,
-      g = {
-        knobs_levels = {}
-      }
-    }
-    knobs = require("knobs")
-    function mockUse(args)
-      return args 
+        describe(
+            "use",
+            function()
+                it(
+                    "when no knobs set",
+                    function()
+                        local use = knobs.use(mockUse)
+                        assert.are.same(use({"no-knob"}), {"no-knob"})
+                    end
+                )
+                it(
+                    "when knob set",
+                    function()
+                        local use = knobs.use(mockUse)
+                        setKnob("thing")
+                        assert.are.same(use({"x/thing.nvim"}), {"x/thing.nvim", cond = 'vim.g["knob_thing"]'})
+                    end
+                )
+            end
+        )
     end
-    function setKnob(knob)
-      vim.g["knob_"..knob] = 1
-      vim.g["knobs_levels"][knob] = 1
-    end
-  end)
-
-  describe("from package name", function()
-    it("when nvim after", function()
-      assert.are.equal(knobs.fromPackage("x/x.nvim"),"x")
-    end)
-    it("when vim after", function()
-      assert.are.equal(knobs.fromPackage("x/x.vim"),"x")
-    end)
-    it("when nvim before", function()
-      assert.are.equal(knobs.fromPackage("x/nvim-x"),"x")
-    end)
-    it("when vim before", function()
-      assert.are.equal(knobs.fromPackage("x/vim-x"),"x")
-    end)
-  end)
-  
-  describe("use", function()
-    it("when no knobs set", function()
-      local use = knobs.use(mockUse)
-      assert.are.same(use({"no-knob"}), {"no-knob"})
-    end)
-    it("when knob set", function()
-      local use = knobs.use(mockUse)
-      setKnob("thing")
-      assert.are.same(use({"x/thing.nvim"}), 
-        {"x/thing.nvim", cond = 'vim.g["knob_thing"]' })
-    end)
-  end)
-end)
-
-
+)
