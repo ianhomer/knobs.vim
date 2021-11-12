529e45a9c732cb34a97d2696ef964d4a8f0a7f2c
Ian Homer <ian.homer@gmail.com>
2021-11-12 23:06:17 +0000

    Switch to using docker to test not podman

--- test/container.sh
+++ test/container.sh
@@ -5,8 +5,5 @@
 
 _DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 cd $_DIR/..
-podman build -t local/arch-me -f Dockerfile .
-# As per https://githubmemory.com/repo/containers/podman/issues/11837
-# need to specify a port others it fails with podman. -p 8080 can be removed
-# soon when that's fixed
-podman run -p 8080 --rm -it local/arch-me
+docker build -t local/arch-me -f Dockerfile .
+docker run --rm -it local/arch-me
